9e4e3811da29f5365a548caa17c1abd3e090b3f0
Fajar <fajar.ariev@gmail.com>
2022-09-10 10:21:11 +0700

    layout

---
+++ MVC/NorthwindSolution/Northwind.Web/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace Northwind.Web.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
---
+++ MVC/NorthwindSolution/Northwind.Web/Repository/IEmployee.cs
@@ -0,0 +1,9 @@
+﻿using Northwind.Web.Models;
+
+namespace Northwind.Web.Repository
+{
+    public interface IEmployee
+    {
+        public List<Employee> GetAll();
+    }
+}
